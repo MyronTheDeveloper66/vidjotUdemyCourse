1614e10484a39bcbf87bcde2c8fcb69d7f5f470c
Myron Castillo <myroncastillo1@yahoo.com>
2017-12-05 19:10:27 -0600

    added new files, extensions, and modifications. Added schema as well

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -13,11 +14,19 @@ mongoose.connect('mongodb://localhost/vidjot-dev', {
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
 
+// Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
 // Handlebars Middleware
 app.engine('handlebars', exphbs({ 
     defaultLayout: 'main' 
 }));
 app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Index Route
 app.get('/', (req, res) => {
@@ -27,10 +36,37 @@ app.get('/', (req, res) => {
     });
 });
 
-// About Rout
+// About Route
 app.get('/about', (req, res) => {
     res.render("about");
 });
+
+// Add Idea From
+app.get('/ideas/add', (req, res) => {
+    res.render("ideas/add");
+});
+
+// Process Form
+app.post('/ideas', (req, res) => {
+    let errors = [];
+    if(!req.body.title) {
+        errors.push({text: 'Please add a title'});
+    }
+    if (!req.body.details) {
+        errors.push({ text: 'Please add the details' });
+    } 
+
+    if(errors.length > 0) {
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        })
+    } else {
+        res.send('passed');
+    }
+});
+
 const port = 5000;
 
 app.listen(port, () => {
